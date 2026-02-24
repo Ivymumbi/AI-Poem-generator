1073fe463ef40b57d57d88dc7482edac4de42f46
Ivymumbi <ivymumbi078@gmail.com>
2026-02-24 15:50:18 +0300

    improved user experience

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ function generate(event) {
     "You are a romantic and poem expert and love to write short poems.Your mission is:generate a short poem with six lines in basic html and dont show that you are using html  and sepatare each line with a <br/>. make sure to follow the users instruction.sign the poem with'SheCodes AI'in <strong>element at the bottom and NOT at the top";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">⏳ Generating poem about ${instructionInput.value}</div>`;
+
   axios.get(apiUrl).then(generatePoem);
 }
 
